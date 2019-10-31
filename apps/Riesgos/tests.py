dc77f2d8fb15767f7b75364a0ef58009c2e99307
nbec29 <nbonillae@uqvirtual.edu.co>
2019-10-31 11:46:12 -0500

    Subida prueba unitaria prueba

--- apps/Riesgos/tests.py
+++ apps/Riesgos/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
+from apps.Riesgos.views import poblacionEliminar
+
+class Tests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def testEliminar(self):
+        url = reverse('login')
+        response = self.client.get(url)
+        self.assertContains(response, 'Iniciar Sesión')
 # Create your tests here.
