297d5fd4377618b0e43a574359aeb628eebc1148
nbec29 <nbonillae@uqvirtual.edu.co>
2019-11-07 08:06:46 -0500

    Subida Grupo Usuarios

--- apps/Registro/views.py
+++ apps/Registro/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from apps.Registro.formularios import RegistroUsuario
 from django.urls import reverse
-
+from django.contrib.auth.models import Group
 
 # Create your views here.
 def registro(request):
@@ -19,6 +19,8 @@ class PersonaCreate(CreateView):
     model = User
     form_class = RegistroUsuario
     template_name = 'registro.html'
+    g = Group.objects.get(name = 'Usuarios')
+    g.user_set.add(RegistroUsuario)
 
     def get_success_url(self):
         return reverse('login')
--- apps/Riesgos/tests.py
+++ apps/Riesgos/tests.py
@@ -1,12 +1,45 @@
-from django.contrib.auth.models import User
 from nose.tools import assert_true
-from django.test import SimpleTestCase
-from django.http import HttpRequest
+import requests
 
-class HomePageTests(SimpleTestCase):
-    def test_home(self):
-        response = self.client.get('/login')
-        self.assertEquals(response.status_code, 200)
+BASE_URL = "https://sveonline.herokuapp.com"
 
-def test_numbers_3_4():
-    assert 4 * 3 == 12
+
+class NewUUID():
+
+    def __init__(self, value):
+        self.value = value
+
+
+def test_request_login():
+    response = requests.get('%s/login' % BASE_URL)
+    assert_true(response.ok)
+
+
+def test_request_logout():
+    response = requests.get(
+        '%s/logout' % BASE_URL)
+    assert_true(response.ok)
+
+def test_get_notificaciones():
+    response = requests.get(
+        '%s/inicio/notificaciones' % BASE_URL)
+    assert_true(response.ok)
+
+def test_get_poblacion():
+    response = requests.get(
+        '%s/inicio/administrarSVE/poblacionSVE' % BASE_URL)
+    assert_true(response.ok)
+
+def test_get_administrar():
+    response = requests.get(
+        '%s/inicio/administrarSVE' % BASE_URL)
+    assert_true(response.ok)
+
+def test_get_editar():
+       response = requests.get(
+          '%s/inicio/administrarSVE/poblacionSVE/editar/1' % BASE_URL)
+       assert_true(response.ok)
+
+def test_get_individual_request_404():
+    response = requests.get('%s/request/an_incorrect_id' % BASE_URL)
+    assert_true(response.status_code == 404)
